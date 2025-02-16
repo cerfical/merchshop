d841d14dab755d36883e1c9e7196b9519ae473f9
cerfical <138404639+cerfical@users.noreply.github.com>
2025-02-16 16:42:44 +0500

    feat: add input data validation

--- internal/service/model/user.go
+++ internal/service/model/user.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"fmt"
+	"regexp"
+)
+
 type Username string
 
 type UserID int
@@ -40,17 +45,57 @@ type User struct {
 	Coins NumCoins
 }
 
+// TODO: Hardcoded validation parameters?
+
+var userRegex = regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+
 func NewUsername(s string) (Username, error) {
-	// TODO: Add validation
+	if err := validateString(s); err != nil {
+		return "", err
+	}
+
+	if !userRegex.MatchString(s) {
+		return "", Error("only letters, digits and underscores are allowed")
+	}
 	return Username(s), nil
 }
 
+var passwdRegex = regexp.MustCompile(`^[[:print:]]+$`)
+
 func NewPassword(s string) (Password, error) {
-	// TODO: Add validation
+	if err := validateString(s); err != nil {
+		return "", err
+	}
+
+	if !passwdRegex.MatchString(s) {
+		return "", Error("only printable characters are allowed")
+	}
 	return Password(s), nil
 }
 
+func validateString(s string) error {
+	const minLen = 8
+	const maxLen = 128
+
+	switch {
+	case s == "":
+		return Error("must not be empty")
+	case len(s) < minLen:
+		return Error(fmt.Sprintf("must be at least %d characters long", minLen))
+	case len(s) > maxLen:
+		return Error(fmt.Sprintf("must be no more than %d characters long", maxLen))
+	default:
+		return nil
+	}
+}
+
 func NewNumCoins(n int) (NumCoins, error) {
-	// TODO: Add validation
-	return NumCoins(n), nil
+	switch {
+	case n == 0:
+		return 0, Error("must not be zero")
+	case n < 0:
+		return 0, Error("must not be negative")
+	default:
+		return NumCoins(n), nil
+	}
 }
