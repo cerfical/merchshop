302534f82213d15d251cb596578f67b9183c1db1
cerfical <138404639+cerfical@users.noreply.github.com>
2025-02-16 21:34:33 +0500

    refactor: minor improvements

--- internal/service/auth/auth_service.go
+++ internal/service/auth/auth_service.go
@@ -41,29 +41,17 @@ func (s *authService) AuthUser(username model.Username, passwd model.Password) (
 		return "", fmt.Errorf("hash password: %w", err)
 	}
 
-	// TODO: Think of a better solution? Limit the number of attempts?
-	var u *model.User
-	for {
-		u, err = s.users.GetUser(username)
+	// TODO: Think of a better solution?
+	u, err := s.users.GetUser(username)
+	if err != nil {
 		if errors.Is(err, model.ErrUserNotExist) {
-			// Proceed to creating a new user
 			u, err = s.users.CreateUser(username, passwdHash, defaultCoinBalance)
-			if errors.Is(err, model.ErrUserExist) {
-				// Some other transaction created a user with the given username before us,
-				// try to retrieve the user yet another time
-				// In the absence of user deletions, that should be OK,
-				// but otherwise there is a chance, that the user will be deleted before we access it,
-				// so here is the loop
-				continue
-			} else if err != nil {
-				return "", fmt.Errorf("create new user: %w", err)
+			if err != nil {
+				return "", fmt.Errorf("create user: %w", err)
 			}
-		} else if err != nil {
-			return "", fmt.Errorf("get user: %w", err)
+		} else {
+			return "", fmt.Errorf("find user by username: %w", err)
 		}
-
-		// Success: the user does exist and there were no errors in retrieving it
-		break
 	}
 
 	if err := s.hasher.VerifyPassword(passwd, u.PasswordHash); err != nil {
